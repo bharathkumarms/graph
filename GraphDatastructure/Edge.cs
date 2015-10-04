1bfa9a6ea026b63b2a7bdc648bcdd4fa78742807
bharathkumarms <v.bharathkumar@gmail.com>
2015-10-04 17:34:41 +0530

    Giving the project a better name

---
+++ GraphDatastructure/Edge.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphDS
+{
+    class Edge
+    {
+        public Edge(Vertex from, Vertex to, int weight)
+        {
+            VertexFrom = from;
+            VertexTo = to;
+            Weight = weight;
+        }
+
+        public Vertex VertexFrom { get; set; }
+        public Vertex VertexTo { get; set; }
+        public int Weight { get; set; }
+
+    }
+}
